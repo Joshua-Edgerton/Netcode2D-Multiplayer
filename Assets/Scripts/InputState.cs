b7199b590990c2b0ae416c4b3685256d09dd510c
Joshua Edgerton <43973457+Joshua-Edgerton@users.noreply.github.com>
2023-01-18 19:40:03 -0600

    Initial commit

---
+++ Assets/Scripts/InputState.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputState
+{
+    public int Tick;
+    //Our sent input for movement in a Vector3
+    public Vector3 movementInput;
+    //public Vector3 lookInput;
+}
---
+++ Assets/Scripts/PlayerNetwork.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerNetwork : NetworkBehaviour
+{
+    [SerializeField] private NetworkMovementComponent _playerMovement;
+
+    private void Update()
+    {  
+        if (!IsOwner) return;
+
+        if (IsClient && IsLocalPlayer)
+        { 
+            float moveX = 0f;
+            float moveY = 0f;
+
+            if(Input.GetKey(KeyCode.W)) moveY = +1f;
+            if(Input.GetKey(KeyCode.S)) moveY = -1f;
+            if(Input.GetKey(KeyCode.A)) moveX = -1f;
+            if(Input.GetKey(KeyCode.D)) moveX = +1f;
+
+            Vector3 moveDirection = new Vector3(moveX, moveY, 0f).normalized;
+            _playerMovement.ProcessLocalPlayerMovement(moveDirection);
+
+        } else {
+            _playerMovement.ProcessSimulatedPlayerMovement();
+        }
+    }
+}
