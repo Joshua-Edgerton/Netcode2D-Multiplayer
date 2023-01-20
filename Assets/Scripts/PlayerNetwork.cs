8e98b598ae611cd00e67798c95e8fa8916138134
Joshua Edgerton <43973457+Joshua-Edgerton@users.noreply.github.com>
2023-01-20 00:41:08 -0600

    Local player rotation working

--- Assets/Scripts/PlayerNetwork.cs
+++ Assets/Scripts/PlayerNetwork.cs
@@ -7,14 +7,20 @@ using UnityEngine.InputSystem;
 public class PlayerNetwork : NetworkBehaviour
 {
     [SerializeField] private NetworkMovementComponent _playerMovement;
+    [SerializeField] private Transform aimTransform;
+
+    private void Awake()
+    {
+        aimTransform = transform.Find("AimedParent");
+    }
 
     private void Update()
-    {  
+    {
         if (IsClient && IsLocalPlayer)
-        { 
+        {
+            MousePositionHandler();
             float moveX = 0f;
             float moveY = 0f;
-
             if(Input.GetKey(KeyCode.W)) moveY = +1f;
             if(Input.GetKey(KeyCode.S)) moveY = -1f;
             if(Input.GetKey(KeyCode.A)) moveX = -1f;
@@ -26,5 +32,17 @@ public class PlayerNetwork : NetworkBehaviour
         } else {
             _playerMovement.ProcessSimulatedPlayerMovement();
         }
+    }
+
+    public void MousePositionHandler() 
+    {
+        // Grabs the main camera and does a "screen to world point" on the mouse position
+        Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        // Sets the z axis to 0
+        mouseWorldPosition.z = 0f;
+        Vector3 aimDirection = (mouseWorldPosition - transform.position).normalized;
+        // Convert to a Euler angle
+        float angle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg;
+        aimTransform.eulerAngles = new Vector3(0, 0, angle);
     }
 }
